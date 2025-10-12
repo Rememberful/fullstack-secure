57fb201a785596dd46e648cb66c4ded6acae2802
Aditya Kumar <adii.utsav@gmail.com>
2025-10-12 12:08:05 +0530

    added next day

---
+++ Day31-40/Day35/script.js
@@ -0,0 +1,18 @@
+// let adityaKumar = document.getElementsByClassName("box");
+// console.log(adityaKumar);
+
+// //changing the 3rd box to red
+// adityaKumar[2].style.backgroundColor = "pink";
+
+//Targetting by id
+// document.getElementById("2ndbox").style.backgroundColor = "yellow";
+
+//Query selector
+// document.querySelector(".box").style.backgroundColor = "skyblue";
+
+// Query Selector all
+console.log(document.querySelectorAll(".box"));
+
+document.querySelectorAll(".box").forEach(e=>{
+    e.style.backgroundColor = "greenyellow";
+})
