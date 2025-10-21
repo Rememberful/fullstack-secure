321af3d585cfb3b01ce4a6e3cfbce51ef45506dc
Aditya Kumar <adii.utsav@gmail.com>
2025-10-21 23:12:02 +0530

    added next day

---
+++ Day51-60/Day54/first-app/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      Aditya is a boy!
+    </div>
+  )
+}
+
+export default Footer
