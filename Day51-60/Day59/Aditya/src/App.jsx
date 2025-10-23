c2991ace112867fb1fbf89a24be1d9b6aded704c
Aditya Kumar <adii.utsav@gmail.com>
2025-10-23 16:28:47 +0530

    added next day

---
+++ Day51-60/Day59/Aditya/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [name, setName] = useState("Aditya")
+
+  const handleClick = ()=>{
+    alert("Hey I am clicked");
+  }
+  const handleMouseOver = ()=>{
+    alert("I am mouse over");
+  }
+  const handleChange = (e)=>{
+    setName(e.target.value)
+  }
+
+  return (
+    <>
+      <div className="button">
+        <button onClick={handleClick}>Click me!</button>
+      </div>
+      <div className="red" onMouseOver={handleMouseOver}>
+        I am a red div
+      </div>
+      <input type="text" value={name} onChange={handleChange}/>
+    </>
+  )
+}
+export default App
